46ef73bdccae561f87f93d3b6c82b13da97a1ca8
sfoow <140029767+sfoow2@users.noreply.github.com>
2026-02-21 19:58:13 -0500

    Add files via upload

--- Sandrift.ino
+++ Sandrift.ino
@@ -1,26 +1,16 @@
 #include <Wire.h>
 #include <U8g2lib.h>
 
-/* ================= PINS ================= */
-#define MPU_ADDR 0x68
-
-#define SDA_PIN_MAG 8 
-#define SCL_PIN_MAG 9
-
-#define MPU_PWR_MGMT_1 0x6B
-#define MPU_ACCEL_XOUT_H 0x3B
-
+// Pin definitions
 #define SDA_PIN_SCREEN 6
 #define SCL_PIN_SCREEN 5
 
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(
-  U8G2_R0, SCL_PIN_SCREEN, SDA_PIN_SCREEN, U8X8_PIN_NONE
+int8_t xVelocity = 0;
+int8_t yVelocity = 0;
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(
+  U8G2_R0, U8X8_PIN_NONE
 );
-
-int16_t GlobalLastX = 0;
-int16_t GlobalLastY = 0;
-int16_t GlobalLastZ = 0;
-
 
 void setup()
 {
@@ -31,15 +21,7 @@ void setup()
   setupAccelerometer();
 }
 
-uint8_t FrameCounter = 0;
-
 void loop(){
-  FrameCounter++;
-  if (FrameCounter >= 5){
-    AccelerometerTick();
-    FrameCounter = 0;
-  }
+  AccelerometerTick();
   ScreenTick();
-  delay(50);
 }
-
