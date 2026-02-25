7f7dc098f4908c45aecd044a233e9e93487332f9
sfoow <140029767+sfoow2@users.noreply.github.com>
2026-02-25 13:22:32 -0500

    Update Screen.ino

--- Sandrift/Screen.ino
+++ Sandrift/Screen.ino
@@ -19,24 +19,24 @@ uint8_t screenBuffer[SCREEN_W * SCREEN_H / 8];
 
 uint32_t rngState = 1234567;
 
-inline uint32_t fastRand() {
+inline uint32_t fastRand(){
     rngState ^= rngState << 13;
     rngState ^= rngState >> 17;
     rngState ^= rngState << 5;
     return rngState;
 }
 
-inline void clearScreenBuffer() {
+inline void clearScreenBuffer(){
     memset(screenBuffer, 0, sizeof(screenBuffer));
 }
 
-inline void pushBufferToDisplay() {
+inline void pushBufferToDisplay(){
     u8g2.clearBuffer();
     memcpy(u8g2.getBufferPtr(), screenBuffer, sizeof(screenBuffer));
     u8g2.sendBuffer();
 }
 
-void SetupScreen() {
+void SetupScreen(){
 
     Wire.begin(SDA_PIN_SCREEN, SCL_PIN_SCREEN);
     Wire.setClock(1000000);
@@ -74,34 +74,30 @@ void SetupScreen() {
     }
 }
 
-void ScreenTick() {
-    for (int x = 0; x < floor(pow(abs(xVelocity) + abs(yVelocity),1.2)) + 1; x++){
+void ScreenTick(){
+    for (int x = 0; x < floor(pow(abs(xVelocity) + abs(yVelocity),1.2)) + 1; x++){ //allows for it to render more than one frame to simualte the sand going "faster"
         DoSandTick();
     }
     pushBufferToDisplay();
 }
 
-void DoSandTick()
-{
+void DoSandTick(){
     if (xVelocity == 0 && yVelocity == 0){ 
         return;
     }
 
-    for (int s = 0; s < SANDAMOUNT; s++)
-    {
+    for (int s = 0; s < SANDAMOUNT; s++){
         uint8_t x = ListOFSand[s].x;
         uint8_t y = ListOFSand[s].y;
 
         int nx = x + xVelocity;
         int ny = y + yVelocity;
 
-        if ((unsigned)nx < SCREEN_W && (unsigned)ny < SCREEN_H)
-        {
+        if ((unsigned)nx < SCREEN_W && (unsigned)ny < SCREEN_H){
             uint16_t nIdx = PIXEL_INDEX(nx, ny);
             uint8_t nMask = PIXEL_MASK(ny);
 
-            if (!(screenBuffer[nIdx] & nMask))
-            {
+            if (!(screenBuffer[nIdx] & nMask)){
                 uint16_t cIdx = PIXEL_INDEX(x, y);
                 screenBuffer[cIdx] &= ~PIXEL_MASK(y);
 
@@ -117,21 +113,17 @@ void DoSandTick()
         int dx1, dy1;
         int dx2, dy2;
 
-        if (yVelocity != 0)
-        {
-
+        if (yVelocity != 0){
             dx1 = -1; dy1 = yVelocity;
             dx2 =  1; dy2 = yVelocity;
         }
-        else
-        {
+        else {
 
             dx1 = xVelocity; dy1 = -1;
             dx2 = xVelocity; dy2 =  1;
         }
 
-        if (flip)
-        {
+        if (flip){
             int t;
             t = dx1; dx1 = dx2; dx2 = t;
             t = dy1; dy1 = dy2; dy2 = t;
@@ -140,13 +132,11 @@ void DoSandTick()
         int tx = x + dx1;
         int ty = y + dy1;
 
-        if ((unsigned)tx < SCREEN_W && (unsigned)ty < SCREEN_H)
-        {
+        if ((unsigned)tx < SCREEN_W && (unsigned)ty < SCREEN_H){
             uint16_t idx = PIXEL_INDEX(tx, ty);
             uint8_t mask = PIXEL_MASK(ty);
 
-            if (!(screenBuffer[idx] & mask))
-            {
+            if (!(screenBuffer[idx] & mask)){
                 uint16_t cIdx = PIXEL_INDEX(x, y);
                 screenBuffer[cIdx] &= ~PIXEL_MASK(y);
 
@@ -160,13 +150,11 @@ void DoSandTick()
         tx = x + dx2;
         ty = y + dy2;
 
-        if ((unsigned)tx < SCREEN_W && (unsigned)ty < SCREEN_H)
-        {
+        if ((unsigned)tx < SCREEN_W && (unsigned)ty < SCREEN_H){
             uint16_t idx = PIXEL_INDEX(tx, ty);
             uint8_t mask = PIXEL_MASK(ty);
 
-            if (!(screenBuffer[idx] & mask))
-            {
+            if (!(screenBuffer[idx] & mask)){
                 uint16_t cIdx = PIXEL_INDEX(x, y);
                 screenBuffer[cIdx] &= ~PIXEL_MASK(y);
 
@@ -178,3 +166,4 @@ void DoSandTick()
     }
 
 }
+
